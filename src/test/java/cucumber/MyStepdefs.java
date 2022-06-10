7f37065b476e5739093028bbf4b7dc80378a6949
Silwhoon <59741317+Aratolle@users.noreply.github.com>
2022-06-10 15:42:13 +0100

    Basic working test
    Running the test manually will work, still needs to be automatically detected/ran by springboot however

---
+++ src/test/java/cucumber/MyStepdefs.java
@@ -0,0 +1,22 @@
+package cucumber;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class MyStepdefs extends SpringIntegrationTest {
+
+  @Given("all details are correct")
+  public void all_details_are_correct() {
+
+  }
+
+  @When("the user presses the login button")
+  public void the_user_presses_the_login_button() {
+
+  }
+
+  @Then("their details are accepted and log in")
+  public void their_details_are_accepted_and_log_in() {
+  }
+}
