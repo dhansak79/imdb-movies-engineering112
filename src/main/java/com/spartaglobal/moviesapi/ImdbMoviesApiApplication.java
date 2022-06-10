0681366b826bc1f32966b08ceaebc48ceff52ada
GH <georgeheath.98840@gmail.com>
2022-06-10 14:14:46 +0100

    Pushing the project skeleton

---
+++ src/main/java/com/spartaglobal/moviesapi/ImdbMoviesApiApplication.java
@@ -0,0 +1,13 @@
+package com.spartaglobal.moviesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImdbMoviesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImdbMoviesApiApplication.class, args);
+	}
+
+}
