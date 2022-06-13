ef12be68b88510773f2924700dd62b328b170f56
Youmna Naseem <youmnakhan23@gmail.com>
2022-06-13 12:21:49 +0100

    Write Out the CSV Files and Invalid/Corrupt Data. Updates as per the google style coding.

--- src/main/java/com/spartaglobal/moviesapi/writecsvfile/WriteOutCSVFile.java
+++ src/main/java/com/spartaglobal/moviesapi/writecsvfile/WriteOutCSVFile.java
@@ -8,35 +8,35 @@ import java.util.List;
 public class WriteOutCSVFile {
 
   public static Boolean writeOutCSVFile(List<String[]> films, String writeOutFile) {
-      try
-          (BufferedWriter buffWrite = new BufferedWriter(new FileWriter(writeOutFile, false))) {
-        for(String [] film : films){
-          String writeLine = String.join(",", film);
-          buffWrite.write(writeLine);
-          buffWrite.newLine();
-        }
-        return true;
-
-      } catch (IOException io) {
-        io.printStackTrace();
-        return false;
+    try
+        (BufferedWriter buffWrite = new BufferedWriter(new FileWriter(writeOutFile, false))) {
+      for (String[] film : films) {
+        String writeLine = String.join(",", film);
+        buffWrite.write(writeLine);
+        buffWrite.newLine();
       }
+      return true;
+
+    } catch (IOException io) {
+      io.printStackTrace();
+      return false;
+    }
 
   }
 
-  public static Boolean corruptData(String  films, String writeOutFile){
+  public static Boolean corruptData(String films, String writeOutFile) {
 
-      String writeLine = String.join(",", films);
-      try
-          (BufferedWriter buffWrite = new BufferedWriter(new FileWriter(writeOutFile, false))) {
-        buffWrite.write(writeLine);
-        buffWrite.newLine();
-        return true;
+    String writeLine = String.join(",", films);
+    try
+        (BufferedWriter buffWrite = new BufferedWriter(new FileWriter(writeOutFile, false))) {
+      buffWrite.write(writeLine);
+      buffWrite.newLine();
+      return true;
 
-      } catch (IOException io) {
-        io.printStackTrace();
-        return false;
-      }
+    } catch (IOException io) {
+      io.printStackTrace();
+      return false;
+    }
   }
 }
 
