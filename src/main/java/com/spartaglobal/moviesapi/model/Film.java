f41e60d184075a12a1c3a83722ad87ade323ca13
Ahmad <amadon2g@yahoo.com>
2022-06-14 11:08:43 +0100

    Interim push 2 film

--- src/main/java/com/spartaglobal/moviesapi/model/Film.java
+++ src/main/java/com/spartaglobal/moviesapi/model/Film.java
@@ -6,29 +6,23 @@ import java.util.List;
 import lombok.Data;
 
 @Data
-public class Film {
+public record Film(String title, boolean score, LocalDate year, int duration, String rating,
+  long budget) {}
+//  private Genre genre;
+//  private List<Genre> genres;
+//  private long gross;
+//  private String director;
+//  private String actor1;
+//  private String actor2;
+//  private String actor3;
+//  private String Language;
+//  private String country;
 
-  private String title;
-  private boolean score;
-//  DateTimeFormatter format = new DateTimeFormatterBuilder()
+                   //  DateTimeFormatter format = new DateTimeFormatterBuilder()
 //      .appendPattern("yyyy")
 //      .parseDefaulting(ChronoField.MONTH_OF_YEAR, 1)
 //      .parseDefaulting(ChronoField.DAY_OF_MONTH, 1)
 //      .toFormatter();
 //
 //  private LocalDate year = LocalDate.parse("2008", format);
-  private LocalDate year;
-  private int duration;
-  private String rating;
-  private long budget;
-  private Genre genre;
-  private List<Genre> genres;
-  private long gross;
-  private String director;
-  private String actor1;
-  private String actor2;
-  private String actor3;
-  private String Language;
-  private String country;
-
-}
+//)
