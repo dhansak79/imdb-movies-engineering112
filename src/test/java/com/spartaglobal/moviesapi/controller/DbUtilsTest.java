0f3eaaa91254ea12abc0fd4d2a5033ad937f78b2
Daniel Webb <daniel.webb@me.com>
2022-06-15 19:56:21 +0100

    adding mutation testing, removing redundant tests, renaming test class

--- src/test/java/com/spartaglobal/moviesapi/controller/DbUtilsTest.java
+++
@@ -1,7 +0,0 @@
-package com.spartaglobal.moviesapi.controller;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class DbUtilsTest {
-
-}
--- src/test/java/com/spartaglobal/moviesapi/controller/FilmsRepositoryControllerTest.java
+++ src/test/java/com/spartaglobal/moviesapi/controller/FilmsRepositoryControllerTest.java
@@ -14,7 +14,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @DataJpaTest
 @ActiveProfiles("dev")
-class FilmsRestControllerTest {
+class FilmsRepositoryControllerTest {
 
   @Autowired
   private FilmRepository repo;
