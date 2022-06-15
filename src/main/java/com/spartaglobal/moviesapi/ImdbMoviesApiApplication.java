e65feddc652814bca97f482d6263d3b01443e635
GH <georgeheath.98840@gmail.com>
2022-06-15 14:48:55 +0100

    Pushing changes to project structure

--- src/main/java/com/spartaglobal/moviesapi/ImdbMoviesApiApplication.java
+++ src/main/java/com/spartaglobal/moviesapi/ImdbMoviesApiApplication.java
@@ -8,7 +8,10 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 public class ImdbMoviesApiApplication {
 
   public static void main(String[] args) {
+    String filepath = args[0];
     SpringApplication.run(ImdbMoviesApiApplication.class, args);
+    FileProcessor fileProcessor = new FileProcessor();
+    fileProcessor.process(filepath);
   }
 
 }
