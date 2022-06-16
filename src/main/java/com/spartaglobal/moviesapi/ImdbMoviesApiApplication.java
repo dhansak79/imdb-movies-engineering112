468c8f3a985a84f846a416634eb90757c8158268
Daniel Webb <daniel.webb@me.com>
2022-06-16 12:29:32 +0100

    example api integration test

--- src/main/java/com/spartaglobal/moviesapi/ImdbMoviesApiApplication.java
+++ src/main/java/com/spartaglobal/moviesapi/ImdbMoviesApiApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ImdbMoviesApiApplication {
 
-
   public static void main(String[] args) {
     SpringApplication.run(ImdbMoviesApiApplication.class, args);
   }
---
+++ src/test/java/com/spartaglobal/moviesapi/integration/GetFilmIT.java
@@ -0,0 +1,25 @@
+package com.spartaglobal.moviesapi.integration;
+
+import com.spartaglobal.moviesapi.model.Film;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+public class GetFilmIT {
+
+  @Test
+  public void getFilmById() {
+    RestTemplate restTemplate = new RestTemplate();
+    ResponseEntity<Film> responseEntity =
+        restTemplate.getForEntity("http://localhost:8080/getFilm/1", Film.class);
+    Film film = responseEntity.getBody();
+    Assertions.assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    Assertions.assertEquals("Avatar", film.getTitle());
+  }
+}
