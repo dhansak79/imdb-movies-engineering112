ccec7a333d2feeffb5cd0c13c88b4d759682acef
Youmna Naseem <youmnakhan23@gmail.com>
2022-06-16 11:20:03 +0100

    Added method to search by title

--- src/main/java/com/spartaglobal/moviesapi/controller/FilmsRestController.java
+++ src/main/java/com/spartaglobal/moviesapi/controller/FilmsRestController.java
@@ -3,11 +3,14 @@ package com.spartaglobal.moviesapi.controller;
 import com.spartaglobal.moviesapi.model.Film;
 import com.spartaglobal.moviesapi.service.exceptions.NoFilmsInDatabaseException;
 import com.spartaglobal.moviesapi.service.FilmService;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("")
@@ -26,5 +29,22 @@ public class FilmsRestController {
     Iterable<Film> films;
     films = service.getAllFilms();
     return ResponseEntity.ok(films);
+  }
+
+
+  @GetMapping("/getFilms/search")
+  public ResponseEntity<Iterable<Film>> getAllFilmsByTitle(@RequestParam String title) throws NoFilmsInDatabaseException {
+    Iterable<Film> films;
+    List<Film> matchedFilms = new ArrayList<>();
+    films = service.getAllFilms();
+    for (Film film: films) {
+      String lowerCaseTitle = film.getTitle().toLowerCase();
+      if(lowerCaseTitle.startsWith(title)) {
+        matchedFilms.add(film);
+      } else if (lowerCaseTitle.contains (title) ) {
+        matchedFilms.add(film);
+      }
+    }
+    return ResponseEntity.ok(matchedFilms);
   }
 }
